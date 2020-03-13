a4017a158f8f5691b45481e4cdda5d1cce7ab51d
Joss <joscelyn.lopez@stkbancomerans.com>
2020-03-13 16:39:42 -0600

    Updates

---
+++ precache-manifest.48ce5147966c48517997ccc73981a46a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3863ee5f6e3a07d6439e2ea3573a548d",
+    "url": "/front/index.html"
+  },
+  {
+    "revision": "2ac9c1a8592841fa9de8",
+    "url": "/front/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb83d0c4f73f1700f177",
+    "url": "/front/static/js/2.497aa42e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/front/static/js/2.497aa42e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac9c1a8592841fa9de8",
+    "url": "/front/static/js/main.53fddd07.chunk.js"
+  },
+  {
+    "revision": "8cd1534e71e73225cbf3",
+    "url": "/front/static/js/runtime-main.a94982e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front/static/media/logo.5d5d9eef.svg"
+  }
+]);
